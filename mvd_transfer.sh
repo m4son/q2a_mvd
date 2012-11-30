ce3be00603d066661a6508344f4b8da178e36a7e
Paul Klumpp <paul@klumpp.net>
2012-11-30 02:52:51 +0100

    sleep a little, so q2proded has time to finalize

--- mvd_transfer.sh
+++ mvd_transfer.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 # small transfer script by Shaque 2012-11-22
 mvd="$1"
 game="$2"
@@ -6,7 +6,12 @@ game="$2"
 _file="$game/demos/$mvd"
 
 if [ -f "$_file" ]; then
-	echo "put $_file" | sftp -q -p someuser@aq2world.com &
+  # sleep a little, so q2proded has time to finalize the demo file
+  ( sleep 2 ) && ( echo "put $_file" | sftp -q -p someuser@aq2world.com:/demos ) &
 else
-	echo "$0 Error: '$_file' not found. Not transferring anything. :("
+  echo "$0 Error: '$_file' not found. Not transferring anything. :("
 fi
+
+
+# vim: expandtab tabstop=2 autoindent:
+# kate: space-indent on; indent-width 2; mixedindent off;
