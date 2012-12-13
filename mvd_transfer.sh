60a1f57903af16e6faa37ac93637e9c24b49c150
Paul Klumpp <paul@klumpp.net>
2012-12-13 18:47:04 +0100

    require pub key auth solely so proc won't stuck

--- mvd_transfer.sh
+++ mvd_transfer.sh
@@ -7,7 +7,7 @@ _file="$game/demos/$mvd"
 
 if [ -f "$_file" ]; then
   # sleep a little, so q2proded has time to finalize the demo file
-  ( sleep 2 ) && ( echo "put $_file" | sftp -q -p someuser@aq2world.com:/demos ) &
+  ( sleep 2 ) && ( echo "put $_file" | sftp -q -o PasswordAuthentication=no -p someuser@aq2world.com:/demos ) &
 else
   echo "$0 Error: '$_file' not found. Not transferring anything. :("
 fi
